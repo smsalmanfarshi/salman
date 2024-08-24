9931077f2b69464587167386623f991ca65ecae4
S. M. Salman Farshi <132475130+smsalmanfp@users.noreply.github.com>
2024-08-24 17:01:30 +0600

    salman

---
+++ class-1.php
@@ -0,0 +1,48 @@
+<?php
+echo "I am Ali\n";
+$Name = "Salman";
+
+echo "$Name\n";
+$number1 = 5;
+$number2 = 10;
+
+$sum = $number1 + $number2;
+echo"The result of the sum is $sum\n";
+
+$height = 5.8;
+echo"My height is $height feet\n";
+
+$isMarried = true;
+echo"$isMarried\n";
+
+$cities = ['Dhaka','Sirajganj','Rajshahi'];
+print_r($cities);
+var_dump($cities);
+
+define("SITE_NAME","smsalmanfarshi.com");
+echo  SITE_NAME;
+
+function sum1(){
+  $num1 = 100;
+  $num2 = 200;
+  $total=$num1+$num2;
+  echo $total;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ function.php
@@ -0,0 +1,9 @@
+<?php
+function sum1(){
+  $num1 = 100;
+  $num2 = 200;
+  $total = $num1+$num2;
+  echo $total;
+
+}
+sum1();
